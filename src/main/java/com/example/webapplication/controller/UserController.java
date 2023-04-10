caa756948eec0a9c1a58d390e17a1c5b15fd2bb5
CaoKy.Nguyen <caoky.nguyen.1704@gmail.com>
2023-04-10 15:29:10 +0700

    push

---
+++ src/main/java/com/example/webapplication/controller/UserController.java
@@ -0,0 +1,68 @@
+package com.example.webapplication.controller;
+
+import com.example.webapplication.DTO.request.ChangePasswordRequest;
+import com.example.webapplication.DTO.response.MessageResponse;
+import com.example.webapplication.jwt.JwtUtils;
+import com.example.webapplication.service.UserService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private JwtUtils jwtUtils;
+    @PostMapping("/change-password")
+    public ResponseEntity<?> changePass(@Valid @RequestBody ChangePasswordRequest changePasswordRequest, HttpServletRequest request){
+        try {
+            String jwt = jwtUtils.parseJwt(request);
+            if(jwt !=null){
+                if(jwtUtils.validateJwtToken(jwt)){
+                    if(userService.changePasswordByJWT(jwt,changePasswordRequest)){
+                        return ResponseEntity.ok(new  MessageResponse(0,"Password change!!"));
+                    }
+                }
+            }
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new MessageResponse(400, "Lỗi xác thực"));
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new MessageResponse(400, "Lỗi xác thực"));
+        }
+    }
+    @PostMapping("/profile")
+    public ResponseEntity<?> getProfile(HttpServletRequest request){
+        try {
+            String jwt = jwtUtils.parseJwt(request);
+            if(jwt !=null){
+                if(jwtUtils.validateJwtToken(jwt)){
+                    return ResponseEntity.ok(userService.getProfileUser(jwtUtils.getUserNameFromJwtToken(jwt)));
+                }
+            }
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new MessageResponse(400, "Lỗi xác thực"));
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new MessageResponse(400, "Lỗi xác thực"));
+        }
+    }
+    @RequestMapping(value = "/logout", method = RequestMethod.POST)
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        request.getSession().invalidate();
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                cookie.setValue("");
+                cookie.setPath("/");
+                cookie.setMaxAge(0);
+                response.addCookie(cookie);
+            }
+        }
+
+        return "logout";
+    }
+}
